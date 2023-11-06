03b12552fa53c30c45afa45f8793084b76953eeb
vaguewords <vaguewords@gmail.net>
2023-11-06 18:44:36 +0900

    로그인 0.2.1 / 회원가입 0.2.1
    로그인 0.2.1 / 회원가입 0.2.1

---
+++ src/main/java/metaint/replanet/rest/auth/controller/AuthController.java
@@ -0,0 +1,36 @@
+package metaint.replanet.rest.auth.controller;
+
+
+import metaint.replanet.rest.auth.dto.MemberRequestDto;
+import metaint.replanet.rest.auth.dto.MemberResponseDto;
+import metaint.replanet.rest.auth.dto.TokenDto;
+import metaint.replanet.rest.auth.dto.TokenRequestDto;
+import metaint.replanet.rest.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<MemberResponseDto> signup(@RequestBody MemberRequestDto memberRequestDto) {
+        return ResponseEntity.ok(authService.signup(memberRequestDto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDto> login(@RequestBody MemberRequestDto memberRequestDto) {
+        return ResponseEntity.ok(authService.login(memberRequestDto));
+    }
+
+    @PostMapping("/reissue")
+    public ResponseEntity<TokenDto> reissue(@RequestBody TokenRequestDto tokenRequestDto) {
+        return ResponseEntity.ok(authService.reissue(tokenRequestDto));
+    }
+}

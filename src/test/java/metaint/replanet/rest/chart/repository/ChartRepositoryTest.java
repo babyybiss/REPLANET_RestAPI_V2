cd50a962c7136d2f7e5451c7cc889fcd0182e824
Gray-Grazer <cleit8307@gmail.com>
2023-11-06 18:38:22 +0900

    chart 패키지 생성

---
+++ src/test/java/metaint/replanet/rest/chart/repository/ChartRepositoryTest.java
@@ -0,0 +1,11 @@
+package metaint.replanet.rest.chart.repository;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ChartRepositoryTest {
+
+    private ChartRepository chartRepository;
+
+
+}

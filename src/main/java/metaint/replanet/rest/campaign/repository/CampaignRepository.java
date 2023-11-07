8cfbdf245ea6b428722d811001b685b46635351f
wjs960 <wjs960@gmail.com>
2023-11-07 19:37:06 +0900

    '캠페인리스트'

---
+++ src/main/java/metaint/replanet/rest/campaign/repository/CampaignRepository.java
@@ -0,0 +1,20 @@
+package metaint.replanet.rest.campaign.repository;
+
+import metaint.replanet.rest.campaign.entity.CampaignDescription;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public interface CampaignRepository extends JpaRepository<CampaignDescription, Integer> {
+    List<CampaignDescription> findByEndDateAfter(LocalDateTime currentDate);
+    List<CampaignDescription> findByEndDateBefore(LocalDateTime currentDate);
+
+    @Query(value = "SELECT * FROM tbl_campaign_description WHERE end_date >= current_date ORDER BY end_date desc", nativeQuery = true)
+    List<CampaignDescription> findCampaignSort(Date currentDate);
+
+}

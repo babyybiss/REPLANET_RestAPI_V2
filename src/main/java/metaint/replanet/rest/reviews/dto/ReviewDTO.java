63e07a94b1d4a161af76e8f9b76fbfd6bb30cbf6
Hanna Lee <younghyoun.l@gmail.com>
2023-11-08 21:02:38 +0900

    review refactoring and authentication error solved

--- src/main/java/metaint/replanet/rest/reviews/dto/ReviewDTO.java
+++ src/main/java/metaint/replanet/rest/reviews/dto/ReviewDTO.java
@@ -11,7 +11,7 @@ public class ReviewDTO{
     private String description;
 
     private Long campaignCode;
-    private String campaignTitle;
+   /* private String campaignTitle;
     private String campaignContent;
     private Date startDate;
     private Date endDate;
@@ -21,25 +21,15 @@ public class ReviewDTO{
     private String orgName;
     private String orgDescription;
     private String orgTel;
-
+*/
     public ReviewDTO() {
     }
 
-    public ReviewDTO(Long campaignRevCode, String reviewTitle, String description, Long campaignCode, String campaignTitle, String campaignContent, Date startDate, Date endDate, String campaignCategory, Integer currentBudget, Integer goalBudget, String orgName, String orgDescription, String orgTel) {
+    public ReviewDTO(Long campaignRevCode, String reviewTitle, String description, Long campaignCode) {
         this.campaignRevCode = campaignRevCode;
         this.reviewTitle = reviewTitle;
         this.description = description;
         this.campaignCode = campaignCode;
-        this.campaignTitle = campaignTitle;
-        this.campaignContent = campaignContent;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.campaignCategory = campaignCategory;
-        this.currentBudget = currentBudget;
-        this.goalBudget = goalBudget;
-        this.orgName = orgName;
-        this.orgDescription = orgDescription;
-        this.orgTel = orgTel;
     }
 
     public Long getCampaignRevCode() {
@@ -74,86 +64,6 @@ public class ReviewDTO{
         this.campaignCode = campaignCode;
     }
 
-    public String getCampaignTitle() {
-        return campaignTitle;
-    }
-
-    public void setCampaignTitle(String campaignTitle) {
-        this.campaignTitle = campaignTitle;
-    }
-
-    public String getCampaignContent() {
-        return campaignContent;
-    }
-
-    public void setCampaignContent(String campaignContent) {
-        this.campaignContent = campaignContent;
-    }
-
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(Date startDate) {
-        this.startDate = startDate;
-    }
-
-    public Date getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(Date endDate) {
-        this.endDate = endDate;
-    }
-
-    public String getCampaignCategory() {
-        return campaignCategory;
-    }
-
-    public void setCampaignCategory(String campaignCategory) {
-        this.campaignCategory = campaignCategory;
-    }
-
-    public Integer getCurrentBudget() {
-        return currentBudget;
-    }
-
-    public void setCurrentBudget(Integer currentBudget) {
-        this.currentBudget = currentBudget;
-    }
-
-    public Integer getGoalBudget() {
-        return goalBudget;
-    }
-
-    public void setGoalBudget(Integer goalBudget) {
-        this.goalBudget = goalBudget;
-    }
-
-    public String getOrgName() {
-        return orgName;
-    }
-
-    public void setOrgName(String orgName) {
-        this.orgName = orgName;
-    }
-
-    public String getOrgDescription() {
-        return orgDescription;
-    }
-
-    public void setOrgDescription(String orgDescription) {
-        this.orgDescription = orgDescription;
-    }
-
-    public String getOrgTel() {
-        return orgTel;
-    }
-
-    public void setOrgTel(String orgTel) {
-        this.orgTel = orgTel;
-    }
-
     @Override
     public String toString() {
         return "ReviewDTO{" +
@@ -161,16 +71,6 @@ public class ReviewDTO{
                 ", reviewTitle='" + reviewTitle + '\'' +
                 ", description='" + description + '\'' +
                 ", campaignCode=" + campaignCode +
-                ", campaignTitle='" + campaignTitle + '\'' +
-                ", campaignContent='" + campaignContent + '\'' +
-                ", startDate=" + startDate +
-                ", endDate=" + endDate +
-                ", campaignCategory='" + campaignCategory + '\'' +
-                ", currentBudget=" + currentBudget +
-                ", goalBudget=" + goalBudget +
-                ", orgName='" + orgName + '\'' +
-                ", orgDescription='" + orgDescription + '\'' +
-                ", orgTel='" + orgTel + '\'' +
                 '}';
     }
 }

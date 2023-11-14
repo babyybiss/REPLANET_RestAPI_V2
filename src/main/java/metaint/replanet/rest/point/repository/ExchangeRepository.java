808aca56388b01dff95c05ef37712464199eb642
jiyeong08 <a01086283142@gmail.com>
2023-11-14 09:34:48 +0900

    [Feature] 포인트 내역 조회 0.1.2, 전환 신청 목록 조회 0.2.1

--- src/main/java/metaint/replanet/rest/point/repository/ExchangeRepository.java
+++ src/main/java/metaint/replanet/rest/point/repository/ExchangeRepository.java
@@ -1,14 +1,9 @@
 package metaint.replanet.rest.point.repository;
 
-import metaint.replanet.rest.point.dto.PointHistoryDTO;
 import metaint.replanet.rest.point.entity.Exchange;
-import metaint.replanet.rest.point.entity.PointFile;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
@@ -17,18 +12,4 @@ public interface ExchangeRepository extends JpaRepository<Exchange, Integer> {
     List<Exchange> findByMemberCode(int memberCode);
 
     List<Exchange> findByStatus(String status);
-
-    @Query(value = "SELECT CASE WHEN e.points != 0 THEN e.processing_date ELSE d.donation_date_time END AS changeDate, " +
-            "CASE WHEN e.points != 0 THEN e.title ELSE c.campaign_title END AS content, " +
-            "CASE WHEN e.points != 0 THEN e.points ELSE d.donation_point END AS changePoint, " +
-            "m.current_point AS remainingPoint " +
-            "FROM tbl_member m " +
-            "LEFT JOIN tbl_point_exchange e ON m.member_code = e.member_code " +
-            "LEFT JOIN tbl_donation d ON m.member_code = d.member_code " +
-            "LEFT JOIN tbl_campaign_description c ON d.campaign_code = c.campaign_code " +
-            "WHERE m.member_code = ?1 " +
-            "ORDER BY COALESCE(e.processing_date, d.donation_date_time) DESC"
-            , nativeQuery = true)
-    List<PointHistoryDTO> findHistoryByMemberCode(int memberCode);
-
 }

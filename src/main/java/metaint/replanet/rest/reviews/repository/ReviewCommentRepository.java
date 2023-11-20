7b8d70e40700ce50f62dcc44d051b73a867b3a6d
Hanna Lee <younghyoun.l@gmail.com>
2023-11-20 10:15:23 +0900

    [FEATURE] 관리자가 댓글 삭제시 댓글 상태 변경 0.6.2

--- src/main/java/metaint/replanet/rest/reviews/repository/ReviewCommentRepository.java
+++ src/main/java/metaint/replanet/rest/reviews/repository/ReviewCommentRepository.java
@@ -3,7 +3,10 @@ package metaint.replanet.rest.reviews.repository;
 
 import metaint.replanet.rest.reviews.entity.ReviewComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,6 +18,8 @@ public interface ReviewCommentRepository extends JpaRepository<ReviewComment, Lo
 
     void deleteByReviewCode(Long reviewCode);
 
-    //@Query(value = "")
-    void updateRevCommentMonitorized(Long revCommentCode);
+    @Modifying
+    @Transactional
+    @Query("UPDATE ReviewComment rc SET rc.revCommentMonitorized = 'Y' WHERE rc.revCommentCode = :revCommentCode")
+    void updateRevCommentMonitorized(@Param("revCommentCode") Long revCommentCode);
 }

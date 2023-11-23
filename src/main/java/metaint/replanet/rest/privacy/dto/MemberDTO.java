d7b9567edd0bcf4d78a1113a3b4a6a0b42f1753b
jiyeong08 <a01086283142@gmail.com>
2023-11-23 18:22:59 +0900

    [Feature] 기부금 영수증 0.1.1

---
+++ src/main/java/metaint/replanet/rest/privacy/dto/MemberDTO.java
@@ -0,0 +1,26 @@
+package metaint.replanet.rest.privacy.dto;
+
+import lombok.*;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class MemberDTO {
+
+    private int memberCode;
+    private String memberId;
+    private String memberName;
+    private String password;
+    private String phone;
+    private Date joinDate;
+    private String memberRole;
+    private char withdraw;
+    private Date withdrawDate;
+    private int currentPoint;
+    private char infoConsent;
+    private String residentNum;
+}
